12b481c5905186dd3af2a2f28928c68d179275b3
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-23 20:04:59 +0430

    chore: add nft-holder entity

---
+++ src/entities/nft-holder.entity.js
@@ -0,0 +1,7 @@
+export class NFTHolder {
+  constructor(amount, remainingAmount, tokenID) {
+    this.amount = amount;
+    this.remainingAmount = remainingAmount;
+    this.tokenID = tokenID;
+  }
+}
