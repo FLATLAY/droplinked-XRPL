3763de907456d985e09559a8564e2cee10087a0c
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-23 19:45:35 +0430

    chore: add NFT-Metadata entity

---
+++ src/Entities/nft-metadata.entity.js
@@ -0,0 +1,10 @@
+export class NFTMetadata {
+  constructor(name, description, image, attributes, properties, localization) {
+    this.name = name;
+    this.description = description;
+    this.image = image;
+    this.attributes = attributes;
+    this.properties = properties;
+    this.localization = localization;
+  }
+}
