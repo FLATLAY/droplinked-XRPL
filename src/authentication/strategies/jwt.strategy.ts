01b8371392b002e4f1707fb923a7f77c3769f917
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-30 15:43:44 +0430

    chore: add jwt strategy

---
+++ src/authentication/strategies/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import mongoose from 'mongoose';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from 'src/modules/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { id: string }) {
+    const user = await this.usersService.getUser({
+      _id: new mongoose.Types.ObjectId(payload.id),
+    });
+
+    if (!user)
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.UNAUTHORIZED,
+          status: 'failed',
+          data: { message: 'invalid jwt. user could not be found .' },
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+
+    return user;
+  }
+}
