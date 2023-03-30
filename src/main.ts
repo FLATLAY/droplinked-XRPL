1baee68aad570bade62d5f7e7eb13b1400aa055f
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-30 14:43:20 +0430

    chore: use interceptors globally and read url and port from .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { ExceptionInterceptor } from './utilities/interceptors/exception.interceptor';
+import { ResponseInterceptor } from './utilities/interceptors/response.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.useGlobalInterceptors(new ExceptionInterceptor());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
+  await app.listen(
+    configService.get<number>('APPLICATION_PORT'),
+    configService.get<string>('APPLICATION_URL'),
+  );
+
+  console.log(`application is running: ${await app.getUrl()}`);
 }
 bootstrap();
