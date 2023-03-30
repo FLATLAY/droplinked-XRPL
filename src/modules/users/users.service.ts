7b7aa780c073c7c58468fdf8ae57b979255f2f6b
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-30 14:45:55 +0430

    chore: add create user, get user and update user methods

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { FilterQuery, Model, UpdateQuery } from 'mongoose';
+import { User, UserDocument } from './models/user.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly usersRepo: Model<UserDocument>,
+  ) {}
+
+  async createUser(query: Partial<User>) {
+    return this.usersRepo.create(query);
+  }
+
+  async getUser(filter: FilterQuery<UserDocument>) {
+    return this.usersRepo.findOne(filter);
+  }
+
+  async updateUser(
+    filter: FilterQuery<UserDocument>,
+    update: UpdateQuery<UserDocument>,
+  ) {
+    return this.usersRepo.findOneAndUpdate(filter, update, { new: true });
+  }
+}
