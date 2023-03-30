72a492e0228a888605c53d8db5252e032fcfe781
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-30 15:42:01 +0430

    chore: add signin route handler

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { JoiValidationPipe } from 'src/utilities/validation-pipes/joi.validation-pipe';
 import { AuthenticationService } from './authentication.service';
+import { SigninDTO, SigninSchema } from './dtos/signin.dto';
 import { SignupDTO, SignupSchema } from './dtos/signup.dto';
 
 @Controller('authentication')
@@ -10,5 +11,10 @@ export class AuthenticationController {
   @Post('/signup')
   async signup(@Body(new JoiValidationPipe(SignupSchema)) body: SignupDTO) {
     return this.authenticationService.signup(body);
+  }
+
+  @Post('/signin')
+  async signin(@Body(new JoiValidationPipe(SigninSchema)) body: SigninDTO) {
+    return this.authenticationService.signin(body);
   }
 }
