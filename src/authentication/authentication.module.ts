fe4a455c2b6414d3147b7b21bdd75a2df0867320
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-30 15:42:42 +0430

    chore: import jwt module and jwt strategy

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -3,10 +3,24 @@ import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { UsersModule } from 'src/modules/users/users.module';
 import { XRPLModule } from 'src/modules/xrpl/xrpl.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UsersModule, XRPLModule],
-  providers: [AuthenticationService],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '24 H' },
+      }),
+    }),
+    UsersModule,
+    XRPLModule,
+  ],
+  providers: [AuthenticationService, JwtStrategy],
   controllers: [AuthenticationController],
 })
 export class AuthenticationModule {}
