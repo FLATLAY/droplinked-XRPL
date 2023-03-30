785076cf64d82fa2c98de9ea765f9d438c730b1e
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-30 13:44:19 +0430

    chore: import xrpl helper

--- src/modules/xrpl/xrpl.module.ts
+++ src/modules/xrpl/xrpl.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { XRPLHelper } from './helpers/xrpl.helper';
 import { XRPLService } from './xrpl.service';
 
 @Module({
-  providers: [XRPLService],
+  providers: [XRPLService, XRPLHelper],
 })
 export class XRPLModule {}
