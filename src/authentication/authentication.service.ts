b47673d7e44d3547ee0ab596a97ee5fdc2f548e7
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-03-30 15:43:21 +0430

    chore: add signin and update signup method

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,12 +1,16 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt/dist';
+import { UserDocument } from 'src/modules/users/models/user.model';
 import { UsersService } from 'src/modules/users/users.service';
 import { XRPLWalletsService } from 'src/modules/xrpl/services/xrpl-wallets.service';
 import { Wallet } from 'xrpl';
+import { SigninDTO } from './dtos/signin.dto';
 import { SignupDTO } from './dtos/signup.dto';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
+    private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
     private readonly xrplWalletsService: XRPLWalletsService,
   ) {}
@@ -26,6 +30,21 @@ export class AuthenticationService {
 
     if (user) throw new BadRequestException('user already exists .');
 
-    return this.usersService.createUser({ xrplWalletSeed: wallet.seed });
+    return this.usersService.createUser({
+      xrplWalletSeed: wallet.seed,
+      xrplClassicAddress: wallet.classicAddress,
+    });
+  }
+
+  async signin({ xrplClassicAddress }: SigninDTO) {
+    const user = await this.usersService.getUser({ xrplClassicAddress });
+
+    if (!user) throw new BadRequestException('user does not exists .');
+
+    return this.createJWT(user);
+  }
+
+  async createJWT(user: UserDocument) {
+    return { jwt: this.jwtService.sign({ id: user.id }) };
   }
 }
