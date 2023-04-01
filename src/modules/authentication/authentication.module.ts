1cf8fcf229695938871484a403d664f15dcadbd4
Hasan Safari <80105137+hasan5afari@users.noreply.github.com>
2023-04-01 11:20:43 +0430

    chore: import offline strategy

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -6,6 +6,7 @@ import { XRPLModule } from 'src/modules/xrpl/xrpl.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { OfflineStrategy } from './strategies/offline.strategy';
 
 @Module({
   imports: [
@@ -20,7 +21,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     UsersModule,
     XRPLModule,
   ],
-  providers: [AuthenticationService, JwtStrategy],
+  providers: [AuthenticationService, JwtStrategy, OfflineStrategy],
   controllers: [AuthenticationController],
 })
 export class AuthenticationModule {}
